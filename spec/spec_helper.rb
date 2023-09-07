55041a498ac3ff0d89de91561d670cf015df5c38
Ng Chen Hon <chenhon.ng@kaligo.com>
2023-09-07 17:20:22 +0800

    make spec environment runnable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,32 @@
 require 'dotenv'
 Dotenv.load!
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'shrine/plugins/rom'
-require 'shrine'
-require 'shrine/storage/file_system'
+require 'pry'
 require 'rom'
 require 'rom/sql'
-require_relative 'database_helper'
-require_relative 'rom_setup/all'
-require_relative 'persistence/all'
+require 'rom-repository'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'shrine'
+require 'shrine/storage/file_system'
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new(Dir.tmpdir),
   store: Shrine::Storage::FileSystem.new("spec/tmp", prefix: 'uploads')
 }
 
+require_relative 'database_helper'
+require 'support/persistence/all'
 
+DatabaseHelper.new.migrate
+
+binding.pry
+
+RSpec.configure do |config|
+  config.after(:each) do |_example|
+    Repositories::KittenRepository.new.clear
+    Repositories::MultiCatRepository.new.clear
+    Repositories::PluginsModelRepository.new.clear
+  end
+end
